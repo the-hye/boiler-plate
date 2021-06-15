d8b4cd1c5d2e3b13ff38a9d2ebc3d1145fea4588
the-hye <ganzi410@gmail.com>
2021-06-15 17:54:48 +0900

    raw register

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jeje:abcd@boiler.iydwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -11,5 +19,17 @@ mongoose.connect('mongodb+srv://jeje:abcd@boiler.iydwm.mongodb.net/myFirstDataba
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    //회원 가입 필요 정보 client to database
+
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
