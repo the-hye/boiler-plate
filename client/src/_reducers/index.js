ed1ee0314505ef025a9477dbdd30908fe262c7fd
the-hye <ganzi410@gmail.com>
2021-06-23 13:56:47 +0900

    redux

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,10 @@
+// rootReducer에 Reducer들을 합체
+
+import { combineReducers } from "redux";
+// import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    //user,
+})
+
+export default rootReducer;
